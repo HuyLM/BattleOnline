ff2650b859f3658b8b98bddd7efce6ccb556b7e8
huylm <lemanhhuy.khmt@gmail.com>
2021-02-09 21:13:49 +0700

    test physic

---
+++ Assets/Game/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    [SerializeField] private Rigidbody myRigi;
+    [SerializeField] private float forceValue;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            Push();
+        }
+    }
+
+    private void Push()
+    {
+        myRigi.velocity = Vector3.forward * forceValue;
+    }
+}
